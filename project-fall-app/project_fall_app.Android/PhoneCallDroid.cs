38413a06d95c89ac56e627b88bedd44c24bd0bea
Carsten Ibsen <lille.bob@hotmail.com>
2017-11-29 15:30:33 +0100

    Make A call

---
+++ project-fall-app/project_fall_app.Android/PhoneCallDroid.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Telephony;
+using Android.Views;
+using Android.Widget;
+using project_fall_app.InterFace;
+
+namespace project_fall_app.Droid
+{
+    class PhoneCallDroid : IMakeCall
+    {
+
+        public void MakeQuickCall(string phoneNumber)
+        {
+            try
+            {
+
+                var uri = Android.Net.Uri.Parse(string.Format("tel:{0}", phoneNumber));
+                var intent = new Intent(Intent.ActionCall, uri);
+                Xamarin.Forms.Forms.Context.StartActivity(intent);
+
+                AudioManager audioManager = (AudioManager)Android.App.Application.Context.GetSystemService(Context.AudioService);
+                audioManager.Mode = Mode.InCall;
+                audioManager.SpeakerphoneOn = true;
+            }
+            catch (Exception e)
+            {
+                new AlertDialog.Builder(Xamarin.Forms.Forms.Context).SetPositiveButton("Ok", (sender, args) =>
+                    {
+                        //TODO what should happen
+                    })
+                    .SetMessage(e.ToString())
+                    .SetTitle("Android Error")
+                    .Show();
+            }
+        }
+    }
+}
---
+++ project-fall-app/project_fall_app/InterFace/IMakeCall.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project_fall_app.InterFace
+{
+    interface IMakeCall
+    {
+        void MakeQuickCall(string phoneNumber);
+    }
+}
